a19e2dea03773bec5d377c3896ec1854bd28f8b1
Maksim Ochenashko <maxochenashko@gmail.com>
2021-06-01 13:33:06 +0300

    Cleanup logs

--- src/main/scala/io/github/irevive/client/DockerComposeClient.scala
+++ src/main/scala/io/github/irevive/client/DockerComposeClient.scala
@@ -12,15 +12,11 @@ object DockerComposeClient {
 
   object Docker extends DockerComposeClient("docker") {
 
-    def up(dockerComposeFile: String, projectName: String, envVars: List[(String, String)]): Unit = {
-      println(s"docker compose -f $dockerComposeFile -p $projectName up -d")
+    def up(dockerComposeFile: String, projectName: String, envVars: List[(String, String)]): Unit =
       Process(s"docker compose -f $dockerComposeFile -p $projectName up -d", None, envVars: _*).!
-    }
 
-    def down(dockerComposeFile: String, projectName: String, envVars: List[(String, String)]): Unit = {
-      println(s"docker compose -f $dockerComposeFile -p $projectName down")
+    def down(dockerComposeFile: String, projectName: String, envVars: List[(String, String)]): Unit =
       Process(s"docker compose -f $dockerComposeFile -p $projectName down", None, envVars: _*).!
-    }
 
   }
 
@@ -35,9 +31,9 @@ object DockerComposeClient {
   }
 
   def create: Either[Throwable, DockerComposeClient] =
-    Try(Process("docker compose -h").!)
+    Try(Process("docker compose ls").!!)
       .map(_ => Docker)
-      .orElse(Try(Process("docker-compose -h").!).map(_ => DockerCompose))
+      .orElse(Try(Process("docker-compose --help").!!).map(_ => DockerCompose))
       .toEither
 
 }
